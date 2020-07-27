4440901f9ea8db52df9fe823160ee126d571811d
dyf <843347859@qq.com>
2020-07-27 20:30:19 +0800

    update

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
 dd:
-	dd if=mbr.bin of=hd.img bs=512 count=1 conv=notrunc
+	nasm -I ./asm/include -o mbr.bin ./asm/mbr.s
+	nasm -I ./asm/include -o loader.bin ./asm/loader.s
+	dd if=./bin/mbr.bin of=hd.img bs=512 count=1 conv=notrunc
+	dd if=./bin/loader.bin of=hd.img bs=512 count=1 seek=2 conv=notrunc
+run:
+	bochs -q
--- asm/mbr.s
+++ asm/mbr.s
@@ -1,4 +1,7 @@
 ; Main Boot Record
+
+%include "boot.inc"
+
 section MBR vstart=0x7c00
 mov ax, cs
 mov ds, ax
@@ -6,6 +9,8 @@ mov es, ax
 mov ss, ax
 mov fs, ax
 mov sp, 0x7c00
+mov ax, 0xb800
+mov gs, ax
 
 ; Clear screen
 ;-----------------
@@ -19,21 +24,100 @@ mov dx, 0x184f
 int 0x10
 
 ; Get cursor
-mov ah, 3
-mov bh, 0
-int 0x10
+;mov ah, 3
+;mov bh, 0
+;int 0x10
 
-; Print string
-mov ax, message
-mov bp, ax
-mov cx, 12       ; string length
-mov ax, 0x1301
-mov bx, 0x2
-int 0x10
+mov bx, 0
+mov cx, 0
+mov ah, 0x03  ; Set color
 
-jmp $
+call put_string
 
-message db "Hello, world"
+mov eax, LOADER_START_SECTOR
+mov bx, LOADER_BASE_ADDR
+mov cx, 1
+call rd_disk_m_16
+
+jmp LOADER_BASE_ADDR
+
+;-----------------
+; Read a sector from disk
+rd_disk_m_16:
+; eax= LBA number, bx = dest address, cx = read sector number
+;-----------------
+    mov esi, eax    ; copy eax
+    mov di, cx      ; copy cx
+
+; 1. set sector number
+    mov dx, 0x1f2
+    mov al, cl
+    out dx, al
+    mov eax, esi
+
+    mov dx, 0x1f3
+    out dx, al
+
+    mov cl, 8
+    shr eax, cl
+    mov dx, 0x1f4
+    out dx, al
+
+    shr eax, cl
+    mov dx, 0x1f5
+    out dx, al
+
+    shr eax, cl
+    and al, 0x0f
+    or al, 0xe0
+    mov dx, 0x1f6
+    out dx, al
+
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+.not_ready:
+    nop
+    in al, dx
+    and al, 0x88
+    cmp al, 0x08
+    jnz .not_ready
+
+    mov ax, di
+    mov dx, 256
+    mul dx
+    mov cx, ax
+
+    mov dx, 0x1f0
+
+.go_on_read:
+    in ax, dx
+    mov [bx], ax
+    add bx, 2
+    loop .go_on_read
+    ret
+
+; jmp $
+
+put_string:
+    mov di, zxy
+    add di, cx
+    mov al, [di]
+    call put_char
+    add cx, 1
+    cmp cx, 20
+    jl put_string
+    ret
+
+put_char:
+    mov byte [gs:bx], al
+    add bx, 1
+    mov byte [gs:bx], ah
+    add bx, 1
+    ret
+
+zxy db "dyf love pwn forever"
 times 510-($-$$) db 0
 db 0x55, 0xaa
 
