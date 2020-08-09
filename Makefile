54dc15a3abe2e01b34ae3a424d4a152f2f89f798
dyf <843347859@qq.com>
2020-08-09 13:28:10 +0800

    update

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 dd:
-	nasm -I ./asm/include -o mbr.bin ./asm/mbr.s
-	nasm -I ./asm/include -o loader.bin ./asm/loader.s
+	nasm -I ./asm/include -o ./bin/mbr.bin ./asm/mbr.s
+	nasm -I ./asm/include -o ./bin/loader.bin ./asm/loader.s
 	dd if=./bin/mbr.bin of=hd.img bs=512 count=1 conv=notrunc
-	dd if=./bin/loader.bin of=hd.img bs=512 count=1 seek=2 conv=notrunc
+	dd if=./bin/kernel.bin of=hd.img bs=512 count=1 seek=2 conv=notrunc
 run:
 	bochs -q
+
+clean:
+	rm ./bin/loader.bin ./bin/mbr.bin
--- asm/loader.s
+++ asm/loader.s
@@ -3,36 +3,10 @@
 section loader vstart=LOADER_BASE_ADDR
 
 
-mov ax, 0x600
-mov bx, 0x700
-mov cx, 0
-mov dx, 0x184f
-
-int 0x10
-
-mov bx, 0
-mov cx, 0
-mov ah, 0x03  ; Set color
-
-call put_string
+mov sp, 0x900
+mov ax, 0x02
+mov bx, 0x03
+push ax
+push bx
 
 jmp $
-
-put_string:
-    mov di, zxy
-    add di, cx
-    mov al, [di]
-    call put_char
-    add cx, 1
-    cmp cx, 10
-    jl put_string
-    ret
-
-put_char:
-    mov byte [gs:bx], al
-    add bx, 1
-    mov byte [gs:bx], ah
-    add bx, 1
-    ret
-
-zxy db "dyf pwned!"
